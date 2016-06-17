664110e3dd8f5c3142e3a0ad48cc30112f9a72d8
Marcello Seri <marcello.seri@gmail.com>
2016-06-17 18:03:00 +0100

    improved example

--- README.md
+++ README.md
@@ -25,6 +25,11 @@ main = do
     putStrLn $ "Base64: " ++ (encode Base64 m)
 ```
 
+# Test
+
+Some preliminary tests can be performed with `stack test`.
+A simple example encoder is in `app/Main.hs`. You can run it on files (e.g. ) or read data from the standard input (e.g. `echo -n test | stack exec mh -- -`)
+
 # TODO
 - Implement hash checker that takes some data and an encoded multihash and check that the multihash corresponds to the data (inferring automatically the appropriate hash function)
 - Evaluate if throwing an error in the encode function is the wanted behaviour and anyway implement a safe version returning an Either type
--- app/Main.hs
+++ app/Main.hs
@@ -2,29 +2,62 @@
 
 module Main where
 
+import Control.Monad
 import Crypto.Multihash
-import Data.ByteString hiding (putStrLn)
-
-printer :: (HashAlgorithm a, Codable a, Show a) => a -> IO ()
-printer ha = do
-  let m = multihash ha ("test"::ByteString)
-  putStrLn $ "Hashing \"test\" with " ++ (show ha)
-  putStrLn $ "Base16: " ++ (encode Base16 m)
-  -- Base32 missing
-  putStrLn $ "Base58: " ++ (encode Base58 m)
-  putStrLn $ "Base64: " ++ (encode Base64 m)
-  putStrLn ""
-
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as B
+import Data.List
+import System.Console.GetOpt
+import System.IO hiding (withFile)
+import System.Environment
+import System.Exit
+import Text.Printf (printf)
 
 main :: IO ()
 main = do
-  printer SHA1
-  printer SHA256
-  printer SHA512
-  printer SHA3_512
-  printer SHA3_384
-  printer SHA3_256
-  printer SHA3_224
-  printer Blake2b_512
-  printer Blake2s_256
-  putStrLn "Done! Note: shake-128/256 and Base32 are missing"
+  (args, files) <- getArgs >>= parse
+  mapM_ printers files
+  putStrLn "Done! Note: shake-128/256 and Base32 are not yet part of the library"
+ 
+printer :: (HashAlgorithm a, Codable a, Show a) => a -> ByteString -> IO ()
+printer alg bs = do
+  let m = multihash alg bs
+  putStrLn $ printf "Base16: %s" (encode Base16 m)
+  -- Base32 missing
+  putStrLn $ printf "Base58: %s" (encode Base58 m)
+  putStrLn $ printf "Base64: %s" (encode Base64 m)
+  putStrLn ""
+
+printers f = do
+  d <- withFile f
+  putStrLn $ printf "Hashing %s\n" (if f == "-" then show d else show f)
+  printer SHA1 d
+  printer SHA256 d
+  printer SHA512 d
+  printer SHA3_512 d
+  printer SHA3_384 d
+  printer SHA3_256 d
+  printer SHA3_224 d
+  printer Blake2b_512 d
+  printer Blake2s_256 d
+  where withFile f = if f == "-" then B.getContents else B.readFile f
+
+data Flag = Help                  -- --help
+          deriving (Eq,Ord,Enum,Show,Bounded)
+ 
+flags = [Option [] ["help"] (NoArg Help) "Print this help message"]
+ 
+parse argv = case getOpt Permute flags argv of
+    (args,fs,[]) -> do
+        let files = if null fs then ["-"] else fs
+        if Help `elem` args
+            then do hPutStrLn stderr (usageInfo header flags)
+                    exitWith ExitSuccess
+            else return (nub (concatMap set args), files)
+ 
+    (_,_,errs)   -> do
+        hPutStrLn stderr (concat errs ++ usageInfo header flags)
+        exitWith (ExitFailure 1)
+ 
+    where header = "Usage: mh [file ...]"
+          set f  = [f]
